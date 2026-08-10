e60e61048979ff3c0db93e383f8e14601b4ed5bc
Ryan Thomas <ryanthomas@Ryans-MacBook-Air-49.local>
2026-08-09 22:22:38 -0400

    More GUI functionality

--- src/gui/app.py
+++ src/gui/app.py
@@ -1,64 +1,104 @@
 import tkinter as tk
+import asyncio
 
-def setup(root):
-    root.title("MyLM")
-    root.configure(bg="#f3ebdd")
-    root.geometry("450x550+100+100")
-    root.resizable(False, False)
-    root.iconphoto(False, tk.PhotoImage(file="src/assets/logo.icns")) # change during packaging
+class GUI:
 
-def build_heading(frame):
-    text = tk.Text(
-        frame, 
-        bg="#f3ebdd",
-        bd=0,
-        highlightthickness=0,
-        relief="flat",
-        height=11
-    )
-    font_style = "Helvetica Neue"
-    text.insert("end", "My", "1")
-    text.tag_configure(
-        "1",
-        font=(font_style, 55, "italic"),
-        foreground="#1f3a2e",
-    )
-    text.insert("end", "LM", "2")
-    text.tag_configure(
-        "2",
-        font=(font_style, 55, "bold"),
-        foreground="#1f3a2e",        
-    )
-    text.insert("end", " Server", "3")
-    text.tag_configure(
-        "3",
-        font=(font_style, 55),
-        foreground="#1f3a2e",
-    )
-    text.tag_configure("1", justify="center")
-    text.config(state="disabled")
-    text.bind("<Button-1>", lambda event: "break")
-    text.bind("<Double-Button-1>", lambda event: "break")
-    text.bind("<Triple-Button-1>", lambda event: "break")
-    text.bind("<B1-Motion>", lambda event: "break")
-    text.pack()
+    def __init__(self, state, on_start, on_kill, models):
+        self.on_start = on_start
+        self.on_kill = on_kill
+        self.root = tk.Tk()
+        self.state = state # True = START, False = KILL
+        self.busy = False
+        self.frame = tk.Frame(self.root, bg="#f3ebdd", padx=10, pady=50)
+        self.button = tk.Button(
+            self.frame,
+            command=self.handle_click
+        )
+        self.text = tk.Text(
+                self.frame, 
+                bg="#f3ebdd",
+                bd=0,
+                highlightthickness=0,
+                relief="flat",
+                height=3
+            )
+        self.localhost_textbox = tk.Text(
+                self.frame, 
+                bg="#f3ebdd",
+                bd=0,
+                highlightthickness=0,
+                relief="flat",
+                height=2,
+                pady=20
+            )
+        self.lan_textbox = tk.Text(
+                self.frame, 
+                bg="#f3ebdd",
+                bd=0,
+                highlightthickness=0,
+                relief="flat",
+                height=2,
+                pady=20
+            )
+        self.models = models
+        self.selected = tk.StringVar(value=self.models[0])
+        self.dropdown = tk.OptionMenu(
+            self.frame,
+            self.selected,
+            *self.models
+        )
+        
+        # high level setup
+        GUI.setup(self.root)
 
-def build_model_selection(active, frame, model_list):
-    if active:
-        # build "Model: "
+        # frame setup
+        self.frame.pack(fill="both", expand=True)
+
+        # frame contents
+        GUI.build_heading(self.frame) # MyLM Server
+
+        # buils dropdown when server not running
+        GUI.build_model_selection(self.state, self.frame, self.text, self.dropdown)
+
+        # hosting info when active
+        GUI.build_hosting_info(self.frame, self.localhost_textbox, "8000", self.lan_textbox, "192.921.20.20")
+        
+        # usage button
+        GUI.build_button(self.frame, self.state, self.button)
+
+    def setup(root):
+        root.title("MyLM")
+        root.configure(bg="#f3ebdd")
+        root.geometry("450x550+100+100")
+        root.resizable(False, False)
+        root.iconphoto(False, tk.PhotoImage(file="src/assets/logo.icns")) # change during packaging
+
+    def build_heading(frame):
         text = tk.Text(
             frame, 
             bg="#f3ebdd",
             bd=0,
             highlightthickness=0,
             relief="flat",
-            height=3
+            height=11
         )
         font_style = "Helvetica Neue"
-        text.insert("end", "Model:", "1")
+        text.insert("end", "My", "1")
         text.tag_configure(
             "1",
-            font=(font_style, 30),
+            font=(font_style, 55, "italic"),
+            foreground="#1f3a2e",
+        )
+        text.insert("end", "LM", "2")
+        text.tag_configure(
+            "2",
+            font=(font_style, 55, "bold"),
+            foreground="#1f3a2e",        
+        )
+        text.insert("end", " Server", "3")
+        text.tag_configure(
+            "3",
+            font=(font_style, 55),
             foreground="#1f3a2e",
         )
         text.tag_configure("1", justify="center")
@@ -69,121 +109,162 @@ def build_model_selection(active, frame, model_list):
         text.bind("<B1-Motion>", lambda event: "break")
         text.pack()
 
-        # build dropdown
-        selected = tk.StringVar(value=model_list[0])
+    def build_model_selection(active, frame, text, dropdown):
+        if active:
+            # build "Model: "
+            font_style = "Helvetica Neue"
+            text.insert("end", "Model:", "1")
+            text.tag_configure(
+                "1",
+                font=(font_style, 30),
+                foreground="#1f3a2e",
+            )
+            text.tag_configure("1", justify="center")
+            text.config(state="disabled")
+            text.bind("<Button-1>", lambda event: "break")
+            text.bind("<Double-Button-1>", lambda event: "break")
+            text.bind("<Triple-Button-1>", lambda event: "break")
+            text.bind("<B1-Motion>", lambda event: "break")
+            text.pack()
 
-        dropdown = tk.OptionMenu(
-            frame,
-            selected,
-            *model_list
-        )
+            # build dropdown
+            dropdown.config(
+                font=("PT Mono", 18),
+                width=40,
+                fg="#1f3a2e",
+            )
+            dropdown["menu"].config(
+                font=("PT Mono", 18),
+                fg="#1f3a2e",
+            )
 
-        dropdown.config(
-            font=("PT Mono", 18),
-            width=40,
+            dropdown.pack()
+
+    def build_button(frame, state, button):
+        if state:
+            text = "Start Server"
+        else:
+            text = "Kill Server"
+        button.config(
+            font=("Helvetica Neue", 30),
             fg="#1f3a2e",
+            text=text
         )
-        dropdown["menu"].config(
-            font=("PT Mono", 18),
-            fg="#1f3a2e",
+        button.place(
+            x=215,
+            y=375,
+            width=200,
+            height=70,
+            anchor="center"
         )
 
-        dropdown.pack()
-
-def build_button(frame, text):
-    button = tk.Button(
-        frame, 
-        text=text
-    )
-    button.config(
-        font=("Helvetica Neue", 30),
-        fg="#1f3a2e"
-    )
-    button.place(
-        x=215,
-        y=375,
-        width=200,
-        height=70,
-        anchor="center"
-    )
-
-def build_hosting_info(active=False, frame=None, local_port="N/A", lan_info="N/A"):
-    if active:
+    def build_hosting_info(frame, localhost_textbox, local_port, lan_textbox, lan_info):
         # build localhost info
-        text = tk.Text(
-            frame, 
-            bg="#f3ebdd",
-            bd=0,
-            highlightthickness=0,
-            relief="flat",
-            height=2,
-            pady=20
-        )
         font_style = "PT Mono"
-        text.insert("end", f"Localhost Port: {local_port}", "1")
-        text.tag_configure(
+        localhost_textbox.insert("end", f"Localhost Port: {local_port}", "1")
+        localhost_textbox.tag_configure(
             "1",
             font=(font_style, 20),
             foreground="#1f3a2e",
         )
-        text.tag_configure("1", justify="center")
-        text.config(state="disabled")
-        text.bind("<Button-1>", lambda event: "break")
-        text.bind("<Double-Button-1>", lambda event: "break")
-        text.bind("<Triple-Button-1>", lambda event: "break")
-        text.bind("<B1-Motion>", lambda event: "break")
-        text.pack()
+        localhost_textbox.tag_configure("1", justify="center")
+        localhost_textbox.config(state="disabled")
+        localhost_textbox.bind("<Button-1>", lambda event: "break")
+        localhost_textbox.bind("<Double-Button-1>", lambda event: "break")
+        localhost_textbox.bind("<Triple-Button-1>", lambda event: "break")
+        localhost_textbox.bind("<B1-Motion>", lambda event: "break")
+        localhost_textbox.pack_forget()
 
         # build lan info
-        text = tk.Text(
-            frame, 
-            bg="#f3ebdd",
-            bd=0,
-            highlightthickness=0,
-            relief="flat",
-            height=2
-        )
-        text.insert("end", f"LAN: {lan_info}", "1")
-        text.tag_configure(
+        lan_textbox.insert("end", f"LAN: {lan_info}", "1")
+        lan_textbox.tag_configure(
             "1",
             font=(font_style, 20),
             foreground="#1f3a2e",
         )
-        text.tag_configure("1", justify="center")
-        text.config(state="disabled")
-        text.bind("<Button-1>", lambda event: "break")
-        text.bind("<Double-Button-1>", lambda event: "break")
-        text.bind("<Triple-Button-1>", lambda event: "break")
-        text.bind("<B1-Motion>", lambda event: "break")
-        text.pack()
+        lan_textbox.tag_configure("1", justify="center")
+        lan_textbox.config(state="disabled")
+        lan_textbox.bind("<Button-1>", lambda event: "break")
+        lan_textbox.bind("<Double-Button-1>", lambda event: "break")
+        lan_textbox.bind("<Triple-Button-1>", lambda event: "break")
+        lan_textbox.bind("<B1-Motion>", lambda event: "break")
+        lan_textbox.pack_forget()
 
-def start_gui():
-    # initial window creation
-    root = tk.Tk()
-    
-    # high level setup
-    setup(root)
+    def update_hosting_info(frame, localhost_textbox, local_port, lan_textbox, lan_info):
+        # build localhost info
+        localhost_textbox.config(state="normal")
+        localhost_textbox.delete("1.0", "end")
+        font_style = "PT Mono"
+        localhost_textbox.insert("end", f"Localhost Port: {local_port}", "1")
+        localhost_textbox.tag_configure(
+            "1",
+            font=(font_style, 20),
+            foreground="#1f3a2e",
+        )
+        localhost_textbox.tag_configure("1", justify="center")
+        localhost_textbox.config(state="disabled")
+        localhost_textbox.bind("<Button-1>", lambda event: "break")
+        localhost_textbox.bind("<Double-Button-1>", lambda event: "break")
+        localhost_textbox.bind("<Triple-Button-1>", lambda event: "break")
+        localhost_textbox.bind("<B1-Motion>", lambda event: "break")
+        localhost_textbox.pack_forget()
 
-    # frame setup
-    frame = tk.Frame(root, bg="#f3ebdd", padx=10, pady=50)
-    frame.pack(fill="both", expand=True)
+        # build lan info
+        lan_textbox.config(state="normal")
+        lan_textbox.delete("1.0", "end")
+        lan_textbox.insert("end", f"LAN: {lan_info}", "1")
+        lan_textbox.tag_configure(
+            "1",
+            font=(font_style, 20),
+            foreground="#1f3a2e",
+        )
+        lan_textbox.tag_configure("1", justify="center")
+        lan_textbox.config(state="disabled")
+        lan_textbox.bind("<Button-1>", lambda event: "break")
+        lan_textbox.bind("<Double-Button-1>", lambda event: "break")
+        lan_textbox.bind("<Triple-Button-1>", lambda event: "break")
+        lan_textbox.bind("<B1-Motion>", lambda event: "break")
+        lan_textbox.pack_forget()
 
-    # frame contents
-    build_heading(frame) # MyLM Server
+    def start_gui(self):
+        self.root.mainloop()
 
-    # buils dropdown when server not running
-    models = ["Qwen 3.8 Max", "Llama", "Mistral"]
-    build_model_selection(False, frame, models)
-
-    # hosting info when active
-    build_hosting_info(True, frame, "8000", "192.921.20.20")
-    
-    # usage button
-    build_button(frame, "Kill Server")
-
-    # launch gui
-    root.mainloop()
-
-
-if __name__ == "__main__":
-    start_gui()
+    def handle_click(self):
+        if self.busy:
+            return
+        # if waiting to start
+        if self.state:
+            self.busy = True
+            self.button.config(state="disabled")
+            self.button.config(text="Working...")
+            self.root.update()
+            local_port, lan_info = asyncio.run(self.on_start())
+            self.button.config(text="Kill Server")
+            self.button.config(state="normal")
+            self.dropdown.pack_forget()
+            self.text.pack_forget()
+            GUI.update_hosting_info(self.frame, self.localhost_textbox, local_port, self.lan_textbox, lan_info)
+            self.localhost_textbox.pack()
+            self.lan_textbox.pack()
+            self.root.update_idletasks()
+            self.root.update()
+            self.state = False
+            self.busy = False
+            return
+        else: # if waiting to kill
+            self.busy = True
+            self.button.config(state="disabled")
+            self.button.config(text="Working...")
+            self.root.update()
+            asyncio.run(self.on_kill())
+            self.button.config(text="Start Server")
+            self.button.config(state="normal")
+            self.localhost_textbox.pack_forget()
+            self.lan_textbox.pack_forget()
+            self.text.pack()
+            self.dropdown.pack()
+            self.root.update_idletasks()
+            self.root.update()
+            self.state = True
+            self.busy = False
+            return
--- src/main.py
+++ src/main.py
@@ -1,3 +1,28 @@
-from gui.app import start_gui
+import asyncio
+from gui.app import GUI
 
-start_gui()
+async def start_server():
+    print("STARTING SERVER")
+    await asyncio.sleep(2)
+    print("STARTED")
+    return(("8001", "192.193.20.30"))
+
+async def kill_server():
+    print("KILLING SERVER")
+    await asyncio.sleep(2)
+    print("KILLED")
+    return("KILLED")
+
+def k2():
+    asyncio.run(start_server())
+
+
+def main():
+    models = ["Qwen 3.8 Max", "Llama"]
+    gui = GUI(True, start_server, kill_server, models)
+    gui.start_gui()
+
+
+if __name__ == "__main__":
+    main()
+
